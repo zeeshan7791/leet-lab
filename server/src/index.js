cdffc90af9fdd4589d36007f017f2a3b2f9a9226
Zeeshan Yousaf <zeeshanento34@gmail.com>
2025-04-23 12:26:49 +0500

    install and import cookie parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,34 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import { PrismaClient } from '@prisma/client';
+import authRoutes from "./routes/auth.routes.js";
 dotenv.config()
 const app=express()
 const prisma = new PrismaClient();
 
 app.use(express.json());
-app.post('/register', async (req, res) => {
-    console.log(req.body,'body-----------------')
-    const { email, username, password } = req.body;
-    try {
-      const user = await prisma.user.create({
-        data: {
-          email,
-          username,
-          password,
-        },
-      });
-      console.log(user,"user----------")
-      res.status(201).json(user,"user register");
-    } catch (error) {
-      res.status(500).json({ error: 'Something went wrong' });
-    }
-  });
+app.use(cookieParser())
   
   app.get('/', (req, res) => {
     res.send('Hello,Guys Welcome to leetlab 🧑‍💻');
   });
 
   const PORT = process.env.PORT || 5000;
+  app.use("/api/v1/auth",authRoutes)
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
