6844736460dd456222e161c94698a99fd54e286b
Zeeshan Yousaf <zeeshanento34@gmail.com>
2025-04-23 12:24:57 +0500

    create routes for auth functions

---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+import express from "express"
+import { check, login, logout, register } from "../controllers/auth.controller.js"
+import { authMiddleware } from "../middlewares/auth.middleware.js"
+
+const authRoutes=express.Router()
+authRoutes.post("/register",register)
+authRoutes.post("/login",login)
+authRoutes.get("/logout",authMiddleware,logout)
+authRoutes.get("/check",authMiddleware,check)
+export default authRoutes
