0b2fdf714707a4fdf03108d3bf325d1728fd4840
Zeeshan <zeeshanento34@gmail.com>
2025-05-09 06:39:14 -0700

    add execute code routes in index

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 import { PrismaClient } from '@prisma/client';
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
+import executeRoutes from "./routes/executeCode.routes.js";
 dotenv.config()
 const app=express()
 const prisma = new PrismaClient();
@@ -18,6 +19,7 @@ app.use(cookieParser())
   const PORT = process.env.PORT || 5000;
   app.use("/api/v1/auth",authRoutes)
   app.use("/api/v1/problems",problemRoutes)
+  app.use("/api/v1/execute-code",executeRoutes)
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
