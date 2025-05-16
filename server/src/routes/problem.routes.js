06f766e0c6513e4cec8c231ead5e4379f8e78414
Zeeshan <zeeshanento34@gmail.com>
2025-05-16 16:52:56 +0500

    create update problem route

--- server/src/routes/problem.routes.js
+++ server/src/routes/problem.routes.js
@@ -9,7 +9,7 @@ const problemRoutes=express.Router()
 problemRoutes.post("/create-problem",authMiddleware,checkAdmin,createProblem)
 problemRoutes.get("/get-all-problems",authMiddleware,getAllProblems)
 problemRoutes.get("/get-problem/:id",authMiddleware,getProblemById)
-problemRoutes.put("update-problem/:id",authMiddleware,checkAdmin,updateProblemById)
+problemRoutes.put("/update-problem/:id",authMiddleware,checkAdmin,updateProblemById)
 problemRoutes.delete("/delete-problem/:id",authMiddleware,deleteProblem)
 problemRoutes.get("get-solved-problems",authMiddleware,getAllProblemsSolvedByUser)
 
