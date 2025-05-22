4f7b491fc0c30021f5335fcc5cd865eae5fced94
Zeeshan <zeeshanento34@gmail.com>
2025-05-22 15:59:59 +0500

    create show playlist page

---
+++ client/src/page/ShowPlaylists.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PlaylistTable from '../components/PlaylistTable'
+
+const ShowPlaylists = () => {
+  return (
+   <>
+    <h2 className="text-center text-2xl font-bold">My Playlists</h2>
+      <PlaylistTable  />
+   </>
+  )
+}
+
+export default ShowPlaylists
