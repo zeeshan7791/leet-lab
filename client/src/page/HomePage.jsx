a258512c6c0043df070c04195988acb12052085f
Zeeshan <zeeshanento34@gmail.com>
2025-05-22 16:01:21 +0500

    pass function as props to problem table

--- client/src/page/HomePage.jsx
+++ client/src/page/HomePage.jsx
@@ -32,7 +32,7 @@ const HomePage = () => {
       </p>
 
       {
-        problems.length > 0 ? <ProblemTable problems={problems}/> : (
+        problems.length > 0 ? <ProblemTable problems={problems} getAllProblems={getAllProblems}/> : (
             <p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
           No problems found
         </p>
