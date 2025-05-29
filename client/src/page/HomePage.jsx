235509d1ad929c3cb6419a50e25ae161dd802f63
Zeeshan <zeeshanento34@gmail.com>
2025-05-29 12:31:19 +0500

    fix delete problem

--- client/src/page/HomePage.jsx
+++ client/src/page/HomePage.jsx
@@ -3,11 +3,12 @@ import { Loader } from "lucide-react";
 import { useProblemStore } from '../store/useProblemStore';
 import ProblemTable from '../components/ProblemTable';
 const HomePage = () => {
-  const { getAllProblems, problems, isProblemsLoading } = useProblemStore();
+  const { getAllProblems, problems, isProblemsLoading,deleteProblemById } = useProblemStore();
 
   useEffect(() => {
     getAllProblems();
   }, [getAllProblems]);
+  
 
   if(isProblemsLoading){
     return (
@@ -32,7 +33,7 @@ const HomePage = () => {
       </p>
 
       {
-        problems.length > 0 ? <ProblemTable problems={problems} getAllProblems={getAllProblems}/> : (
+        problems.length > 0 ? <ProblemTable problems={problems} getAllProblems={getAllProblems} deleteProblemById={deleteProblemById}/> : (
             <p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
           No problems found
         </p>
